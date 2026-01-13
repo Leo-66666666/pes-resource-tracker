a7aba0a2d1a130ca30c964632ef9777b5135e1b0
Leo-66666666 <2304696639@qq.com>
2026-01-13 18:46:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
         USERNAME: 'Leo-66666666',  // 你的GitHub用户名
         REPO: 'pes-resource-data',         // 数据仓库名
         BRANCH: 'main',
-        TOKEN: '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',        // 你的GitHub Token
+        TOKEN: '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',        // 你的GitHub Token
         GIST_ID: 'dc6328fd3c6af8f8e6ef3dee506fe57d'            // 存储所有用户数据的Gist ID
     },
     
